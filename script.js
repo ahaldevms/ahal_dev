3b520731c3f6c33007a7751aee8f7a08256b5393
Ahal Dev MS <ahal.vfx@gmail.com>
2023-04-28 11:43:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,124 @@
+function mo_pho(){
+    document.getElementById("pho").style.color="#40d3ff"
+    document.getElementById("pho").style.scale="1.05"
+    
+}
+function ml_pho(){
+    document.getElementById("pho").style.color="black"
+    document.getElementById("pho").style.scale="1"
+}
+
+function mo_mail(){
+    document.getElementById("mail").style.color="#40d3ff"
+    document.getElementById("mail").style.scale="1.05"
+}
+function ml_mail(){
+    document.getElementById("mail").style.color="black"
+    document.getElementById("mail").style.scale="1"
+}
+
+function insta_col_o(){
+    document.getElementById("insta").style.color="#ff0066"
+    document.getElementById("insta").style.scale="1.5"
+}
+function insta_col_l(){
+    document.getElementById("insta").style.color="black"
+    document.getElementById("insta").style.scale="1"
+}
+
+function fbook_col_o(){
+    document.getElementById("facebook").style.color="#0072ff"
+    document.getElementById("facebook").style.scale="1.5"
+}
+function fbook_col_l(){
+    document.getElementById("facebook").style.color="black"
+    document.getElementById("facebook").style.scale="1"
+}
+function img_contact_o(){
+    document.getElementById("img_contact").style.scale="1.05"
+}
+function img_contact_l(){
+    document.getElementById("img_contact").style.scale="1"
+}
+
+function img_about_o(){
+    document.getElementById("img_about").style.scale="1.05"
+}
+function img_about_l(){
+    document.getElementById("img_about").style.scale="1"
+}
+
+
+function img_home_o(){
+    document.getElementById("img_home").style.scale="1.05"
+}
+function img_home_l(){
+    document.getElementById("img_home").style.scale="1"
+}
+
+function card_1_o(){
+    document.getElementById("card_1").style.scale="1.06"
+    document.getElementById("card_1").style.zIndex="1"
+    document.getElementById("card_1").style.boxShadow="0px 1px 15px #c8c8c8"
+}
+function card_1_l(){
+    document.getElementById("card_1").style.scale="1"
+    document.getElementById("card_1").style.zIndex="0"
+    document.getElementById("card_1").style.boxShadow="0px 0px 0px #ffffff"
+}
+
+function card_2_o(){
+    document.getElementById("card_2").style.scale="1.06"
+    document.getElementById("card_2").style.zIndex="1"
+    document.getElementById("card_2").style.boxShadow="0px 1px 15px #c8c8c8"
+}
+function card_2_l(){
+    document.getElementById("card_2").style.scale="1"
+    document.getElementById("card_2").style.zIndex="0"
+    document.getElementById("card_2").style.boxShadow="0px 0px 0px #ffffff"
+}
+
+function card_3_o(){
+    document.getElementById("card_3").style.scale="1.06"
+    document.getElementById("card_3").style.zIndex="1"
+    document.getElementById("card_3").style.boxShadow="0px 1px 15px #c8c8c8"
+}
+function card_3_l(){
+    document.getElementById("card_3").style.scale="1"
+    document.getElementById("card_3").style.zIndex="0"
+    document.getElementById("card_3").style.boxShadow="0px 0px 0px #ffffff"
+}
+
+function card_4_o(){
+    document.getElementById("card_4").style.scale="1.06"
+    document.getElementById("card_4").style.zIndex="1"
+    document.getElementById("card_4").style.boxShadow="0px 1px 15px #c8c8c8"
+}
+function card_4_l(){
+    document.getElementById("card_4").style.scale="1"
+    document.getElementById("card_4").style.zIndex="0"
+    document.getElementById("card_4").style.boxShadow="0px 0px 0px #ffffff"
+}
+
+function card_5_o(){
+    document.getElementById("card_5").style.scale="1.06"
+    document.getElementById("card_5").style.zIndex="1"
+    document.getElementById("card_5").style.boxShadow="0px 1px 15px #c8c8c8"
+}
+function card_5_l(){
+    document.getElementById("card_5").style.scale="1"
+    document.getElementById("card_5").style.zIndex="0"
+    document.getElementById("card_5").style.boxShadow="0px 0px 0px #ffffff"
+}
+
+function card_6_o(){
+    document.getElementById("card_6").style.scale="1.06"
+    document.getElementById("card_6").style.zIndex="1"
+    document.getElementById("card_6").style.boxShadow="0px 1px 15px #c8c8c8"
+}
+function card_6_l(){
+    document.getElementById("card_6").style.scale="1"
+    document.getElementById("card_6").style.zIndex="0"
+    document.getElementById("card_6").style.boxShadow="0px 0px 0px #ffffff"
+}
+
